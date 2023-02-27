e1d2ed3d4defbde08b7610a2a2b9eef40eb85c56
aokll <pastuhov_1992@mail.ru>
2023-02-27 14:20:00 +0300

    finished_task1

---
+++ HW2/task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+int o = new Random().Next(100,1000);
+String s = o.ToString();
+
+char[] arr = s.ToCharArray();
+
+for(int i = 0; i<arr.Length; i++){
+    if(i == 1) Console.Write(o + " -> " + arr[i] + " ");
+}
