680905f2e297357436d54995f0007d3f408f9de1
aokll <pastuhov_1992@mail.ru>
2023-03-05 15:09:58 +0300

    finished_task3.3

--- HW3/task3/Program.cs
+++ HW3/task3/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+void func(int q)
+{
+    for(int i = 1; i <= q; i++){
+        int res = i*i*i;
+        Console.Write(res + " ");
+    }
+    
+}
+int x = int.Parse(Console.ReadLine()!);
+func(x);
