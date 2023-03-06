9fab9bfec5cba9d1ddca91ddae78d960813b71d1
aokll <pastuhov_1992@mail.ru>
2023-03-06 13:10:53 +0300

    finished_task4,1

---
+++ HW4/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*Задача 25: Напишите цикл, который принимает на вход два числа 
+(A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16*/
+
+int func (int a,int b)
+{
+    int res = 0;
+    for(int i = 0; i < b-1; i++){
+        if(i == 0){
+            res = a*1;
+        }
+        res*=a;
+    }
+    return res;
+}
+int x = int.Parse(Console.ReadLine()!);
+int y = int.Parse(Console.ReadLine()!);
+Console.WriteLine(func(x,y));
