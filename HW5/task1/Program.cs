f5177e886c5de2f05069d88d8e943d77b5eba04b
aokll <pastuhov_1992@mail.ru>
2023-03-12 17:07:24 +0300

    в процессе...

---
+++ HW5/task1/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+ Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2*/
+
+int[] func(int len)
+{
+    int[] arr = new int[len];
+
+    for(int i = 0; i < len; i++)
+    {
+        arr[i] = new Random().Next(100,999);
+    }
+    return arr;
+}
+
+int func2(int[] mass){
+int k = 0;
+for(int i = 0; i < mass.Length; i++)
+{
+    
+    if(mass[i]%2==0){
+        k++;
+    }
+    Console.Write(mass[i] + " ");
+}
+return k;
+}
+
+Console.Write(func(Random().Next(1,5)));
+
---
+++ HW5/task4/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Последняя задача из урока
+
+/*[1,2,3,4,5] -> 5,8,3
+[1,2,3,4] -> 4,6*/
+
