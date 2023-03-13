38cf8720293749bf1d25fd41550a16f77e250c3a
aokll <pastuhov_1992@mail.ru>
2023-03-13 13:36:31 +0300

    задача 5.2 сделана

--- HW5/task2/Program.cs
+++ HW5/task2/Program.cs
@@ -5,3 +5,29 @@
 [3, 7, 23, 12] -> 19
 
 [-4, -6, 89, 6] -> 0*/
+
+int[] func(int len)
+{
+    int[] arr = new int[len];
+
+    for(int i = 0; i < len; i++)
+    {
+        arr[i] = new Random().Next(0,200);
+    }
+    return arr;
+}
+
+int res = 0;
+int o = int.Parse(Console.ReadLine()!);
+int[] mass = func(o);
+
+
+for(int i = 0; i < mass.Length; i++)
+{
+    if(i%2 != 0){
+       res += mass[i];
+    }
+    Console.Write($"{mass[i]}[{i}] ");
+}
+Console.WriteLine();
+Console.Write($"Сумма всех чисел на нечетных позициях = {res}");
